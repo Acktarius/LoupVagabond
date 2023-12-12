3e0d1879983a93f714573d2071cad57000c3cd11
Acktarius <94026250+Acktarius@users.noreply.github.com>
2023-12-12 22:17:51 +0000

    initial

---
+++ proxy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#check ilibrewolf is already running
+lr=$(ps -e | grep -c librewolf)
+if [[ ! ${lr} -eq 0 ]]; then
+zenity --warning \
+--timeout=15 \
+--text="Librewolf is already running,\nyou have to exit first\nbefore running this anonymous instance."
+exit
+else
+#check tor is running
+torstatus=$(systemctl is-active --quiet tor.service)
+echo $torstatus
+if [ -z ${torstatus} ]; then
+zenity --warning \
+--timeout=15 \
+--text="tor service is not running.\nOpen a Terminal window (Ctrl+Alt+T) and type : \n\n\t<b>sudo systemctl restart tor</b>"
+sleep 1
+exit
+else
+proxychains4 librewolf https://www.qwant.com/ 2> /dev/null
+fi
+fi
